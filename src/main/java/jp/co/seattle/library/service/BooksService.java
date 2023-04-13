8dca3175ab74fceae15cec00e8f7f856486076fc
nana <nanako.kizami@metateam.co.jp>
2023-04-13 11:09:57 +0900

    task3_1次コミット

--- src/main/java/jp/co/seattle/library/service/BooksService.java
+++ src/main/java/jp/co/seattle/library/service/BooksService.java
@@ -29,11 +29,12 @@ public class BooksService {
 	 *
 	 * @return 書籍リスト
 	 */
+
 	public List<BookInfo> getBookList() {
 
 		// TODO 書籍名の昇順で書籍情報を取得するようにSQLを修正（タスク３）
 		List<BookInfo> getedBookList = jdbcTemplate.query(
-				"",
+				"SELECT * FROM books ORDER BY title",
 				new BookInfoRowMapper());
 
 		return getedBookList;
